c31a2fed06a46d853d8a2e8aff0da536477a00c7
Bohdan Kopcha <kopchabogdan@gmail.com>
2024-06-20 20:07:30 +0300

    Fixed build

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+nextConfig = {
   trailingSlash: true,
   output: "standalone",
-  async redirects() {
-    return [
-      {
-        source: "/management-dashboard/analytics",
-        destination: "/management-dashboard/analytics/overview",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
